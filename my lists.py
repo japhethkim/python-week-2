a68383a142dab06099140a62c48e4f4ea2216104
japhethkim <japhethkimutai2002@gmail.com>
2024-08-28 15:49:36 +0300

    Add files via upload

---
+++ my lists.py
@@ -0,0 +1,23 @@
+my_list = []
+
+# Append elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert element at the second position
+my_list.insert(1, 15)
+
+# Extend the list
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30
+index = my_list.index(30)
+print(index) 
